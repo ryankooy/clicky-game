9ac5d3196c5d9f63b7e090600b4d1d91a2576d85
Ryan Kooy <rwkooy@gmail.com>
2019-11-07 12:20:03 -0500

    laying foundation

---
+++ clicky-game/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      Hi, Footer.
+    </div>
+  );
+}
+
+export default Footer;
---
+++ clicky-game/src/components/ImgGrid/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ImgGrid extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Hi, ImgGrid.
+      </div>
+    );
+  }
+}
+
+export default ImgGrid;
